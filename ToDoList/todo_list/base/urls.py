fda0693e493485d3e9db498a2b017451bad5ab3e
arshiya merrikh <112122902+arshiyamars@users.noreply.github.com>
2024-04-02 03:28:11 +0330

    add project folder

---
+++ ToDoList/todo_list/base/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path 
+from .views import TaskList , TaskDetail, TaskCreate , TaskUpadte  ,DeleteView , CustomLoginView, RegisterPage
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('',TaskList.as_view(), name="tasks"),
+    path('task/<int:pk>/',TaskDetail.as_view(),name="task"),
+    path('task-create/',TaskCreate.as_view(),name="task-create"),
+    path('task-update/<int:pk>/',TaskUpadte.as_view(),name="task-update"),
+    path('task-delete/<int:pk>/',DeleteView.as_view(),name="task-delete"),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/',RegisterPage.as_view(), name="register-page")
+
+
+
+
+]
+
